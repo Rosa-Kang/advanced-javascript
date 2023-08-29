632aa759ebbaaedffa2f9a14c95d75ca4dd562d1
Rosa Kang <therosessom@gmail.com>
2023-08-29 11:38:36 -0700

    update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,34 @@
-alert('This works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+let inputNum = getInput();
+
+console.log(operator, inputNum);
+let calculationLogic = `${defaultResult} + ${currentResult}`;
+
+outputResult(currentResult, calculationLogic);
+
+function add(a, b) {
+    const result = a + b;
+    return result;
+}
+
+function divide(a, b) {
+    const result = a / b;
+    return result;
+}
+
+function subtract(a, b) {
+    const result = a - b;
+    return result;
+}
+
+function multiply(a, b) {
+    const result = a * b;
+    return result;
+}
+
+function getInput(e) {
+    e.preventDefault();
+    let value = e.target.value();
+    return value;
+}
