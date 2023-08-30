60723e814e26f04b452a9f850af4d3ce2c1dc2f3
Rosa Kang <therosessom@gmail.com>
2023-08-30 09:33:25 -0700

    update function calls

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,5 @@
+//order matters for declaration, but not for the functions. therefore you can place your function to the top or to the bottom upon your style.
+
 const defaultResult = 0;
 let currentResult = defaultResult;
 let inputNum = getInput();
@@ -8,23 +10,19 @@ let calculationLogic = `${defaultResult} + ${currentResult}`;
 outputResult(currentResult, calculationLogic);
 
 function add(a, b) {
-    const result = a + b;
-    return result;
+    return a + b;
 }
 
 function divide(a, b) {
-    const result = a / b;
-    return result;
+    return a / b;
 }
 
 function subtract(a, b) {
-    const result = a - b;
-    return result;
+    return a - b;
 }
 
 function multiply(a, b) {
-    const result = a * b;
-    return result;
+    return a * b;
 }
 
 function getInput(e) {
