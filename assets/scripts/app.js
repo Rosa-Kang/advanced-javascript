b0b33b8937ba618da362c96cb00704e30c546e2a
Rosa Kang <therosessom@gmail.com>
2023-08-30 11:56:05 -0700

    update js files

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,28 +1,57 @@
-//order matters for declaration, but not for the functions. therefore you can place your function to the top or to the bottom upon your style.
+// Order matters for declaration, but not for the functions.
+// Therefore you can place your function to the top or to the bottom upon your style.
+
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-    const enteredNum = getInput();
-    const calcDescription = `${currentResult} + ${enteredNum}`;
-    currentResult = currentResult + enteredNum;
+function getUserNumberInput() {
+    return parseInt(usrInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
     outputResult(currentResult, calcDescription);
 }
 
+function add() {
+    const initialResult = currentResult;
+    const enteredNumber = getUserNumberInput();
+    currentResult += enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
-function divide(a, b) {
-    return a / b;
+function solve(a, b) {
+    numbers.push(a, b);
 }
 
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function getInput(e) {
-    return parseInt(usrInput.value);
-}
+const courseData = {
+    title: 'JavaScript - The Complete Guide',
+    duration: 30,
+    levels: ['Beginner', 'Advanced'],
+};
